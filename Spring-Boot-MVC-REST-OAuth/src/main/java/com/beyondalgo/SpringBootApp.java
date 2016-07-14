a1143f9451d068046575e15b06ba7f39e55f8bd1
kpankaj-cg <pankaj.kalwar@capgemini.com>
2016-07-14 17:47:52 +0530

    First Commit

---
+++ Spring-Boot-MVC-REST-OAuth/src/main/java/com/beyondalgo/SpringBootApp.java
@@ -0,0 +1,13 @@
+package com.beyondalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class SpringBootApp {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp.class, args);
+	}
+}
